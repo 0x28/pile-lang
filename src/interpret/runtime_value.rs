21397a297c92b650abf3c7f873569e259ca3cc77
0x28 <none>
2019-08-05 20:44:42 +0200

    Provide fmt implementation for RuntimeValue

--- src/interpret/runtime_value.rs
+++ src/interpret/runtime_value.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub use crate::lex::Number;
 pub use crate::lex::Operator;
 pub use crate::parse::Expr;
@@ -10,4 +12,24 @@ pub enum RuntimeValue<'a> {
     Identifier(String),
     String(String),
     Boolean(bool),
+}
+
+impl<'a> fmt::Display for RuntimeValue<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RuntimeValue::Function(block) => {
+                let address = block as *const _ as usize;
+
+                write!(f, "function @ 0x{}", address)
+            }
+            RuntimeValue::Operator(o) => write!(f, "function '{}'", o),
+            RuntimeValue::Number(Number::Natural(n)) => write!(f, "{}", n),
+            RuntimeValue::Number(Number::Integer(i)) => write!(f, "{}", i),
+            RuntimeValue::Number(Number::Float(fl)) => write!(f, "{}", fl),
+            RuntimeValue::String(s) => writeln!(f, "{}", s),
+            RuntimeValue::Boolean(true) => writeln!(f, "true"),
+            RuntimeValue::Boolean(false) => writeln!(f, "false"),
+            RuntimeValue::Identifier(_) => Err(std::fmt::Error),
+        }
+    }
 }
