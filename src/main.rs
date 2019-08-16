05b611f141b0f4706e5e48994a8cc012ebaa2508
0x28 <none>
2019-08-16 18:57:40 +0200

    Extract parameter parsing into a custom function and call interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
+mod interpret;
 mod lex;
 mod parse;
 
 use std::fs;
+use std::io::{self, Read};
 use std::process;
+
+fn read_program(args: &Vec<String>) -> String {
+    if args.len() == 1 {
+        let mut buffer = String::new();
+        match io::stdin().read_to_string(&mut buffer) {
+            Err(error) => {
+                eprintln!("stdin: {}", error);
+                std::process::exit(1);
+            }
+            Ok(contents) => contents,
+        };
+        buffer
+    } else if args.len() == 2 {
+        let filename = &args[1];
+        match fs::read_to_string(filename) {
+            Err(error) => {
+                eprintln!("{}: {}", filename, error);
+                std::process::exit(1);
+            }
+            Ok(contents) => contents,
+        }
+    } else {
+        eprintln!("Usage: {} [FILE]", args[0]);
+        process::exit(1);
+    }
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-
-    if args.len() != 2 {
-        eprintln!("Usage: {} FILE", args[0]);
-        process::exit(1);
-    }
-
-    let filename = &args[1];
-    let program_text = match fs::read_to_string(filename) {
-        Err(error) => {
-            eprintln!("{}: {}", filename, error);
-            std::process::exit(1);
-        }
-        Ok(contents) => contents,
-    };
+    let program_text = read_program(&args);
 
     let lexer = lex::Lexer::new(program_text.as_ref());
     let parser = parse::Parser::new(lexer);
@@ -30,4 +45,7 @@ fn main() {
         }
         Ok(ast) => ast,
     };
+
+    let mut interpreter = interpret::Interpreter::new(ast);
+    let value = interpreter.run();
 }
