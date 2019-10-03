f47844b53871013df968291cfbff16cdbcebe15b
0x28 <none>
2019-10-03 16:04:21 +0200

    Use ensure_bool in function apply_if

--- src/interpret/condition.rs
+++ src/interpret/condition.rs
@@ -1,19 +1,13 @@
 use super::runtime_error;
 use super::runtime_value::Function;
-use super::runtime_value::RuntimeValue;
 use super::Interpreter;
 use super::State;
 
 pub fn apply_if(state: &mut State) -> Result<(), String> {
     let stack = &mut state.stack;
-    let condition = runtime_error::ensure_element(stack)?;
+    let condition = runtime_error::ensure_bool(stack)?;
     let else_branch = runtime_error::ensure_function(state)?;
     let if_branch = runtime_error::ensure_function(state)?;
-
-    let condition = match condition {
-        RuntimeValue::Boolean(b) => b,
-        v => return Err(format!("Expected boolean found '{}'", v)),
-    };
 
     if condition {
         match if_branch {
