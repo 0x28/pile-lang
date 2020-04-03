c182c06167fbe5bc9a2c4d0a47c3cf1f6d687671
0x28 <none>
2020-04-03 23:36:10 +0200

    Generate README.md from tests
    The readme is now generated using some example programs from the interpreter
    tests. This way the readme will always show working programs.

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+use std::fs;
+
+macro_rules! readme {
+    () => {"\
 [![License: GPL](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 [![pipeline status](https://gitlab.com/0x28/pile-lang/badges/master/pipeline.svg)](https://gitlab.com/0x28/pile-lang/pipelines)
 [![coverage report](https://gitlab.com/0x28/pile-lang/badges/master/coverage.svg)](https://0x28.gitlab.io/pile-lang/)
@@ -10,39 +14,30 @@ A simple stack-oriented toy programming language.
 
 ## Fibonacci
 ```
-begin
-  -> number # arg1 = number
-  0 -> current
-  1 -> next
-  begin
-    next
-    current next + -> next
-    -> current
-  end
-  number
-  dotimes
-
-  current
-end
--> fib
-
+{fib}
 ```
 
 ## Factorial
 ```
-begin
-  -> n
-  1 -> result
-  begin
-    n result * -> result
-    n 1 - -> n
-  end
-  begin
-    n 0 >
-  end
-  while
-  result
-end
--> fact
-
+{fact}
 ```
+"
+    };
+}
+
+fn main() -> std::io::Result<()> {
+    fs::write(
+        "README.md",
+        format!(
+            readme!(),
+            fib = fs::read_to_string(
+                "src/interpret/file_test/proj_fibonacci/fibonacci.pile"
+            )?,
+            fact = fs::read_to_string(
+                "src/interpret/file_test/proj_factorial/factorial.pile"
+            )?
+        ),
+    )?;
+
+    Ok(())
+}
