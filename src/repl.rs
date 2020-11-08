39c2faff1e0ccb7a84d0fc4bf1c0bbb692b79272
0x28 <none>
2020-11-08 20:59:51 +0100

    Use rustyline in pile repl

--- src/repl.rs
+++ src/repl.rs
@@ -5,23 +5,55 @@ use crate::parse::Parser;
 use crate::program_source::ProgramSource;
 use crate::using;
 
-use std::io::Write;
+use std::path::PathBuf;
 use std::rc::Rc;
+
+use rustyline::error::ReadlineError;
+use rustyline::{Config, EditMode, Editor};
+
+fn pile_history() -> PathBuf {
+    let home = dirs::home_dir().unwrap_or(PathBuf::from("."));
+
+    home.join(".pile_history")
+}
+
+fn create_editor() -> Editor<()> {
+    let config = Config::builder().edit_mode(EditMode::Emacs).build();
+    let mut editor = Editor::with_config(config);
+    let _ignored = editor.load_history(&pile_history());
+
+    editor
+}
+
+fn get_line(editor: &mut Editor<()>) -> Result<String, i32> {
+    match editor.readline("pile > ") {
+        Ok(line) => {
+            editor.add_history_entry(line.as_str());
+            Ok(line)
+        }
+        Err(ReadlineError::Eof) => Err(0),
+        Err(ReadlineError::Interrupted) => Err(1),
+        Err(err) => {
+            eprintln!("readline failed: {:?}", err);
+            Err(2)
+        }
+    }
+}
 
 pub fn repl() -> ! {
     let mut interpreter = Interpreter::empty();
-    let mut stdout = std::io::stdout();
-    let stdin = std::io::stdin();
+    let mut editor = create_editor();
+    let exit_code;
 
     loop {
-        print!("pile > ");
-        stdout.flush().expect("stdout flush failed!");
-        let mut line = String::new();
-        stdin.read_line(&mut line).expect("stdin read failed!");
-        if line.is_empty() {
-            println!();
-            break;
-        }
+        let line = match get_line(&mut editor) {
+            Ok(line) => line,
+            Err(code) => {
+                exit_code = code;
+                break;
+            }
+        };
+
         let lexer = Lexer::new(&line, Rc::new(ProgramSource::Repl));
         let parser = Parser::new(lexer);
         let ast = match parser.parse() {
@@ -47,5 +79,13 @@ pub fn repl() -> ! {
         };
     }
 
-    std::process::exit(0);
+    if let Err(err) = editor.save_history(&pile_history()) {
+        eprintln!(
+            "Couldn't save pile history to {}: {:?}",
+            pile_history().to_string_lossy(),
+            err
+        );
+    }
+
+    std::process::exit(exit_code);
 }
